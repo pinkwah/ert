b281e6c050095a0c2bf1045309cff18e8f4671e2
Zohar Malamant <ZOM@equinor.com>
2025-04-07 15:29:18 +0200

    Add devenv.nix

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, config, inputs, ... }:
+
+let
+  python = pkgs.python312;
+in {
+  languages.python = {
+    enable = true;
+    package = python;
+    uv.enable = true;
+    uv.sync.enable = true;
+    uv.sync.allExtras = true;
+    venv.enable = true;
+  };
+
+  env = {
+    PYTHONPATH = "${python.pkgs.pyqt6}/${python.sitePackages}";
+    QT_PLUGIN_PATH = "${config.devenv.profile}/${pkgs.qt6.qtbase.qtPluginPrefix}";
+    XDG_CONFIG_DIRS = "${config.devenv.state}";
+  };
+
+  packages = with pkgs; [
+    libz
+    qt6.qtbase
+    qt6.qtsvg
+    qt6.qtwayland
+
+    # Theming
+    qadwaitadecorations-qt6
+    kdePackages.plasma-integration
+  ];
+}
